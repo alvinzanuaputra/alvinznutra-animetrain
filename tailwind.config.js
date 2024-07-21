6cdcc25b26643673744002b0c27186999fe19468
alvinzanuaputra <faronjaya705@gmail.com>
2024-07-21 15:38:34 +0700

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        gabarito: ['Gabarito', 'sans-serif'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
     colors: {
@@ -18,9 +20,9 @@ module.exports = {
         primary: '#eeeeee',
         accent: '#ffc639',
         secondary: '#393e46',
-        dark: '#222831'
-      }
-    }
+        dark: '#222831',
+      },
+    },
   },
   plugins: [],
 }
