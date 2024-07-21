f84aa62ee8b5643aae4000f13085f17b91dbee13
alvinzanuaputra <faronjaya705@gmail.com>
2024-07-21 18:04:52 +0700

    plis bisa

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        gabarito: ['Gabarito', 'sans-serif'],
-      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
     colors: {
@@ -20,9 +18,9 @@ module.exports = {
         primary: '#eeeeee',
         accent: '#ffc639',
         secondary: '#393e46',
-        dark: '#222831',
-      },
-    },
+        dark: '#222831'
+      }
+    }
   },
   plugins: [],
 }
